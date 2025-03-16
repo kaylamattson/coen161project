579a60e25d0b13d8325d45f7763db000ab3e19cb
Kayla Huffman <khuffman@scu.edu>
2025-03-16 11:21:31 -0700

    kaylah madeVersion.js connections game working

--- fullGame.php
+++ fullGame.php
@@ -44,6 +44,23 @@
     $group3 = $game['group3']['items'];
     $group4 = $game['group4']['items'];
 
+    //insert the groupNames for when a connection is successful
+    $success = $dom->getElementById('success1');
+    $success->textContent = $game['group1']['group'];
+    $success->setAttribute('data-name',$game['group1']['group']);
+    
+    $success = $dom->getElementById('success2');
+    $success->textContent = $game['group2']['group'];
+    $success->setAttribute('data-name',$game['group2']['group']);
+
+    $success = $dom->getElementById('success3');
+    $success->textContent = $game['group3']['group'];
+    $success->setAttribute('data-name',$game['group3']['group']);
+
+    $success = $dom->getElementById('success4');
+    $success->textContent = $game['group4']['group'];
+    $success->setAttribute('data-name',$game['group4']['group']);
+
     $cardArray = array_merge($group1, $group2, $group3, $group4);
     shuffle($cardArray);
 
@@ -53,19 +70,22 @@
         $buttonEl = $dom->createElement('button',$card);
         $buttonEl->setAttribute('type','button');
         $buttonEl->setAttribute('class', 'box');
-        $buttonEl->setAttribute('id', 'card-' . md5($card)); //give each button a unique id
         
         if (in_array($card, $group1)) {
             $buttonEl->setAttribute('data-group-id', '1');
+            $buttonEl->setAttribute('data-group-name', $game['group1']['group']);
         }
         elseif (in_array($card, $group2)) {
             $buttonEl->setAttribute('data-group-id', '2');
+            $buttonEl->setAttribute('data-group-name', $game['group2']['group']);
         }
         elseif (in_array($card, $group3)) {
             $buttonEl->setAttribute('data-group-id', '3');
+            $buttonEl->setAttribute('data-group-name', $game['group3']['group']);
         }
         elseif (in_array($card, $group4)) {
             $buttonEl->setAttribute('data-group-id', '4');
+            $buttonEl->setAttribute('data-group-name', $game['group4']['group']);
         }
 
         $cardContainer->appendChild($buttonEl);
--- madeVersion.js
+++ madeVersion.js
@@ -6,25 +6,49 @@ let clickedCards = [];
 let completedCount = 0;
 let numWins = 0;
 let lastCardId = 0;
+let lastCardGroupName = '';
+let mistakesRemaining = 4;
 
 // turn data-cards into an array for each card
 
 function cardClicked(event) {
-    //switch card background color
-    if (clickedCards.length >= 4) {
-        submitConnection();
+    // if clicked card is already in the array, remove the active class and remove it from clickedCards
+    const clickedCardText = event.currentTarget.textContent;
+    const cardIndex = clickedCards.findIndex(card => card.text === clickedCardText);
+    if (cardIndex !== -1) {
+        // if card is found, remove it (deselect)
+        console.log("deselected");
+        clickedCards.splice(cardIndex, 1);
+        event.currentTarget.classList.remove("active");
+        const submitBtn = document.getElementById('submit-btn');
+        submitBtn.classList.remove('active');
         return;
+    } else {
+        // if card is not found, add it (select)
+        console.log("add card");
+        if (clickedCards.length >= 4) {
+            return;
+        }
+        event.currentTarget.classList.add("active");
     }
 
-    event.currentTarget.classList.toggle("active");
-    const clickedCardText = event.currentTarget.textContent;
     const cardGroupId = event.target.dataset.groupId;
+    const cardGroupName = event.target.dataset.groupName;
     const cardInfo = {
         text: clickedCardText,
-        groupId: cardGroupId
+        groupId: cardGroupId,
+        groupName: cardGroupName
     }
     lastCardId = cardGroupId;
+    lastCardGroupName = cardGroupName;
     clickedCards.push(cardInfo);
+    console.log("clicked cards length", clickedCards.length);
+    if (clickedCards.length >= 4) {
+        console.log("NO MOE SPACE");
+        const submitBtn = document.getElementById('submit-btn');
+        submitBtn.classList.toggle("active");
+        return;
+    }
 
     /*
     console.log("clickedCardsData", clickedCardsData);
@@ -36,34 +60,184 @@ function cardClicked(event) {
 
 // check if each element in the connection matches the correct answer. if yes, add
 function submitConnection() {
-    const cards = document.querySelectorAll('box');
+    const submitBtn = document.getElementById('submit-btn');
+    submitBtn.classList.toggle("active");
     console.log("last card id", lastCardId);
     console.log("clickedCards", clickedCards);
-    
+    deselect();
+
     for (let i = 0; i < clickedCards.length; i++) {
         const card = clickedCards[i];
         
         if (card.groupId !== lastCardId) {
             console.log("failed");
+            mistake();
+            clickedCards = [];
             return;
         }
     }
-
+    successfulConnection();
     // increase number of wins
     numWins += 1;
     console.log("numWins", numWins);
     // remove all cards and restart for the next group
     clickedCards = [];
     completedCount++;
+
+    // game over
+    if (numWins == 4) {
+        gameOver();
+    }
+}
+
+function winGame() {
+
+}
+
+function gameOver() {
+    /*
+    document.getElementById('cardContainer').style.display = "none";
+    document.getElementById('mistakes').style.display = "none";
+    document.getElementById('resultsContainer').style.display = "block";
+    */
+    document.getElementById('success1').style.display = "block";
+    document.getElementById('success2').style.display = "block";
+    document.getElementById('success3').style.display = "block";
+    document.getElementById('success4').style.display = "block";
+    document.getElementById('mistakes').style.display = "none";
+    document.getElementById('results-button-container').style.display = "block";
+    document.querySelectorAll('.box').forEach(box => {
+        box.style.display = 'none';
+    });
+    if (numWins == 4) {
+        document.getElementById('title').textContent = "Great Job!";
+    }
+    if (mistakesRemaining == 0) {
+        document.getElementById('title').textContent = "Next Time!";
+    }
+
+    document.getElementById('completed').textContent = numWins;
+    if (numWins == 0) {
+        document.getElementById('completed').textContent = 0;
+    }
+    document.getElementById('winPercent').textContent = (numWins/4)*100;
+}
+
+function viewResults() {
+    document.getElementById('success1').style.display = "none";
+    document.getElementById('success2').style.display = "none";
+    document.getElementById('success3').style.display = "none";
+    document.getElementById('success4').style.display = "none";
+    document.getElementById('cardContainer').style.display = "none";
+    document.getElementById('mistakes').style.display = "none";
+    document.getElementById("results-button-container").style.display = "none";
+    document.getElementById('resultsContainer').style.display = "block";
+
+}
+
+function deselect() {
+    clickedCards.forEach(card => {
+        // Find the corresponding card element using its text content
+        const cardElements = document.querySelectorAll('.box');
+        cardElements.forEach(cardElement => {
+            if (cardElement.textContent.trim() === card.text.trim()) {
+                cardElement.classList.remove('active');
+            }
+        });
+    });
+}
+
+
+function deselectBtn() {
+    const submitBtn = document.getElementById('submit-btn');
+    submitBtn.classList.remove('active');
+    clickedCards.forEach(card => {
+        // Find the corresponding card element using its text content
+        const cardElements = document.querySelectorAll('.box');
+        cardElements.forEach(cardElement => {
+            if (cardElement.textContent.trim() === card.text.trim()) {
+                cardElement.classList.remove('active');
+            }
+        });
+    });
+    clickedCards = [];
+}
+
+// toggle active class on each card off
+function deselectBtn() {
+    const submitBtn = document.getElementById('submit-btn');
+    submitBtn.classList.remove('active');
+    clickedCards.forEach(card => {
+        // Find the corresponding card element using its text content
+        const cardElements = document.querySelectorAll('.box');
+        cardElements.forEach(cardElement => {
+            if (cardElement.textContent.trim() === card.text.trim()) {
+                cardElement.classList.remove('active');
+            }
+        });
+    });
+    clickedCards = [];
+}
+
+function shuffle() {
+    const container = document.getElementById('cardContainer');
+    const boxes = Array.from(container.getElementsByClassName('box'));
+
+    for (let i = boxes.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [boxes[i], boxes[j]] = [boxes[j], boxes[i]]; // Swap elements
+    }
+    boxes.forEach(box => container.appendChild(box));
+}
+
+function successfulConnection() {
+    console.log("successful connection");
+    const allCards = document.querySelectorAll('.box');
+    allCards.forEach(card => {
+        if (card.dataset.groupName === lastCardGroupName) {
+            card.remove();
+        }
+    })
+    const success = document.querySelector(`[data-name="${lastCardGroupName}"]`);
+    document.getElementById(success.id).style.display = "block";
+}
+
+function mistake() {
+    mistakesRemaining -= 1;
+    if (mistakesRemaining > 0) {
+        const mistakeTally = document.getElementById('circle');
+        mistakeTally.remove();
+        console.log("mistakesRemaining", mistakesRemaining);
+    }
+    else if (mistakesRemaining == 0) {
+        gameOver();
+    }
 }
 
 function initialize() {
     console.log('initializing madeVersion.js')
+    // hide successful connections
+    document.getElementById('success1').style.display = "none";
+    document.getElementById('success2').style.display = "none";
+    document.getElementById('success3').style.display = "none";
+    document.getElementById('success4').style.display = "none";
+    document.getElementById('resultsContainer').style.display = "none";
+    document.getElementById('results-button-container').style.display = "none";
     const cards = document.querySelectorAll('.box');
     cards.forEach(card => {
         card.addEventListener("click", cardClicked);
-         
     });
+    const submit = document.getElementById('submit-btn');
+    submit.addEventListener("click", submitConnection);
+
+    const resultsPressed = document.getElementById('results-btn');
+    resultsPressed.addEventListener("click", viewResults);
+
+    const shufflePressed = document.getElementById('shuffle-btn');
+    shufflePressed.addEventListener("click", shuffle);
+
+    const deselectPressed = document.getElementById('deselect-btn');
+    deselectPressed.addEventListener("click", deselectBtn);
 
     // const cardClicked = document.getElementById('card');
     /*
